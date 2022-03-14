29945cd8e87f1fdc9d2ff3502f32d991e00574ce
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-03-14 11:57:13 +0100

    update on slicing

--- src/Data/Slice.hs
+++ src/Data/Slice.hs
@@ -2,7 +2,7 @@
 
 module Data.Slice where
 
-import Data.Maybe(fromMaybe)
+import Data.Maybe(fromMaybe, maybe)
 import Data.Range(Range)
 
 data Slice a =
@@ -17,11 +17,39 @@ showSlice (Slice sa sb sc) = go sa (':' : go sb (':' : go sc ""))
   where go Nothing = id
         go (Just x) = (show x ++)
 
+getIndex :: Num a => a -> Maybe a -> Maybe a
+getIndex n = fmap go
+  where go i | i < 0 = n + i
+             | otherwise = i
+
 getNumStep :: Num a => Slice a -> a
 getNumStep = fromMaybe 1 . slStep
 
+isReverse :: (Num a, Ord a) => Slice a -> Bool
+isReverse = (0 >) . getNumStep
+
+isForward :: (Num a, Ord a) => Slice a -> Bool
+isForward = (0 <) . getNumStep
+
+getLowerUpper :: Num a => Slice a -> a -> Maybe (a, a)
+getLowerUpper sl n
+  | step < 0 = Just (-1, n-1)
+  | step > 0 = Just (0, n)
+  | otherwise = Nothing
+  where step = getNumStep
+
 getIndices :: Integral a => Slice a -> a -> Maybe (a, a, a)
-getIndices = undefined
+getIndices sl@{ slFrom=f, slTo=t } n
+  | stepDir = Nothing  -- forward
+  | step < 0 = Nothing  -- backward
+  | otherwise = Nothing
+  where step = getNumStep sl
+        stepDir = step > 0
+        lower = bool (-1) 0 stepDir
+        upper = n + lower
+        ~(sta, sto) = bool id swap stepDir (lower, upper)
+        start = fromMaybe sta (getIndex n f)
+        stop = fromMaybe sto (getIndex n t)
 
 -- stepNegative :: Num a => Slice a -> a
 
