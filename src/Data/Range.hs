91f1e69a8b92cb62635c99abea145991a0ad2994
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-03-28 19:09:15 +0200

    instances to determine the length and indices of a Range object

--- src/Data/Range.hs
+++ src/Data/Range.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+
 module Data.Range where
 
+import Data.Indicable(Indicable((!), (!?)))
 import Data.Slice(Slice, getIndices)
 
 data Range a
@@ -21,14 +24,32 @@ rangeFromToStep = Range
 rangeFromThenTo :: Num a => a -> a -> a -> Range a
 rangeFromThenTo x1 x2 xn = Range x1 xn (x2-x1)
 
-integralNumberOfItems :: Integral a => Range a -> Maybe a
-integralNumberOfItems Range { rStart=s, rEnd=e, rStep=st } = Just (div (e-s) st)
+integralNumberOfItems' :: Integral a => Range a -> a
+integralNumberOfItems' ~(Range { rStart=s, rEnd=e, rStep=st }) = div (e-s) st
 
-realFracNumberOfItems :: (RealFrac a, Integral b) => Range a -> Maybe b
-realFracNumberOfItems Range { rStart=s, rEnd=e, rStep=st } = Just (floor ((e-s) / st))
+realFracNumberOfItems' :: (RealFrac a, Integral b) => Range a -> b
+realFracNumberOfItems' ~(Range { rStart=s, rEnd=e, rStep=st }) = floor ((e-s) / st)
 
--- items :: Num a => Range a -> Range a
--- items =
+integralNumberOfItems :: Integral a => Range a -> a
+integralNumberOfItems = max 0 . integralNumberOfItems'
+
+realFracNumberOfItems :: (RealFrac a, Integral b) => Range a -> b
+realFracNumberOfItems = max 0 . realFracNumberOfItems'
+
+elementAt :: (Num a, Integral i) => Range a -> i -> a
+elementAt ~(Range s _ st) i = s + fromIntegral i * st
+
+instance {-# Overlapping #-} Integral a => Indicable Range a where
+  (!) = elementAt
+  (!?) r i
+    | i < 0 || i >= fromIntegral (integralNumberOfItems' r) = Nothing
+    | otherwise = Just (r ! i)
+
+instance {-# Overlappable #-} RealFrac a => Indicable Range a where
+  (!) = elementAt
+  (!?) r i
+    | i < 0 || i >= fromIntegral (realFracNumberOfItems' r) = Nothing
+    | otherwise = Just (r ! i)
 
 sliceRange :: Num a => Slice a -> Range a -> Range a
 sliceRange sl Range { rStart=rS, rEnd=rE, rStep=rSt } = undefined
